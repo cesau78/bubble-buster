7cc75f6244b08774dab32c34bd832d10bafab7df
cesau78 <charles.esau@gmail.com>
2022-09-18 08:17:38 -0400

    Added demo for react-timer-hook

--- src/App.js
+++ src/App.js
@@ -1,22 +1,42 @@
+import { useTimer } from 'react-timer-hook';
+
 import logo from './logo.svg';
 import './App.css';
 
+
 function App() {
+  const time = new Date();
+  time.setMinutes(time.getMinutes() + 20);
+  const {
+    seconds,
+    minutes,
+    hours,
+    days,
+    isRunning,
+    start,
+    pause,
+    resume,
+    restart,
+  } = useTimer({expiryTimestamp: time});
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <h1>react-timer-hook </h1>
+      <p>Timer Demo</p>
+      <div style={{fontSize: '100px'}}>
+        <span>{days}</span>:<span>{hours}</span>:<span>{minutes}</span>:<span>{seconds}</span>
+      </div>
+      <p>{isRunning ? 'Running' : 'Not running'}</p>
+      <button onClick={start}>Start</button>
+      <button onClick={pause}>Pause</button>
+      <button onClick={resume}>Resume</button>
+      <button onClick={() => {
+        // Restarts to 20 minute timer
+        const time = new Date();
+        time.setMinutes(time.getMinutes() + 20);
+        restart(time)
+      }}>Restart</button>
       </header>
     </div>
   );
