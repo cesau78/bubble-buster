e77631e3716278aecce59fc9fd6adf9f943dff02
cesau78 <charles.esau@gmail.com>
2022-09-19 05:23:03 -0400

    linting applied

--- src/App.js
+++ src/App.js
@@ -1,9 +1,8 @@
-import { useTimer } from 'react-timer-hook';
+import { useTimer } from "react-timer-hook";
 
-import './App.css';
+import "./App.css";
 
-import {TimerDisplay} from "./components/TimerDisplay";
-
+import { TimerDisplay } from "./components/TimerDisplay";
 
 function App() {
   const time = new Date();
@@ -18,24 +17,34 @@ function App() {
     pause,
     resume,
     restart,
-  } = useTimer({expiryTimestamp: time});
+  } = useTimer({ expiryTimestamp: time });
 
   return (
     <div className="App">
       <header className="App-header">
-      <h1>react-timer-hook </h1>
-      <p>Timer Demo</p>
-      <TimerDisplay className="Timer" days={days} hours={hours} minutes={minutes} seconds={seconds} />
-      <p>{isRunning ? 'Running' : 'Not running'}</p>
-      <button onClick={start}>Start</button>
-      <button onClick={pause}>Pause</button>
-      <button onClick={resume}>Resume</button>
-      <button onClick={() => {
-        // Restarts to 20 minute timer
-        const time = new Date();
-        time.setMinutes(time.getMinutes() + 20);
-        restart(time)
-      }}>Restart</button>
+        <h1>react-timer-hook </h1>
+        <p>Timer Demo</p>
+        <TimerDisplay
+          className="Timer"
+          days={days}
+          hours={hours}
+          minutes={minutes}
+          seconds={seconds}
+        />
+        <p>{isRunning ? "Running" : "Not running"}</p>
+        <button onClick={start}>Start</button>
+        <button onClick={pause}>Pause</button>
+        <button onClick={resume}>Resume</button>
+        <button
+          onClick={() => {
+            // Restarts to 20 minute timer
+            const time = new Date();
+            time.setMinutes(time.getMinutes() + 20);
+            restart(time);
+          }}
+        >
+          Restart
+        </button>
       </header>
     </div>
   );
